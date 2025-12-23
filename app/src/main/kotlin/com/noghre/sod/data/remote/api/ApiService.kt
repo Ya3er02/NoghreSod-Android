3f4ef66020d134575bf1bbb60ab666b84224b612
Yaser <64164851+Ya3er02@users.noreply.github.com>
2025-12-23 23:15:55 +0330

    Fix import statement in ApiService

--- app/src/main/kotlin/com/noghre/sod/data/remote/api/ApiService.kt
+++ app/src/main/kotlin/com/noghre/sod/data/remote/api/ApiService.kt
@@ -8,7 +8,7 @@ import retrofit2.http.POST
 import retrofit2.http.PUT
 import retrofit2.http.Path
 import retrofit2.http.Query
-from com.noghre.sod.data.dto.*
+import com.noghre.sod.data.dto.*
 
 /**
  * Retrofit API Service Interface
